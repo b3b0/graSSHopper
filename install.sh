21eae60014cfc981d3ae6b4eaebfa633ca55aa0d
b3b0 <b3b0@users.noreply.github.com>
2018-04-25 19:42:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 mkdir /usr/share/grasshopper/ 2> /dev/null
 cp * /usr/share/grasshopper
 touch /usr/bin/grasshopper
-printf "#!/bin/bash\nruby /usr/share/grasshopper/graSSHopper.rb" > /usr/bin/grasshopper
+cp grasshopper /usr/bin/grasshopper
 chmod 777 /usr/bin/grasshopper
 rm /usr/share/grasshopper/install.sh
 echo "Thanks bro"
