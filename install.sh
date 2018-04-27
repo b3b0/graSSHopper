49c97f9e540273ad0387180f029bbfa491b68b3d
b3b0 <b3b0@users.noreply.github.com>
2018-04-27 10:39:19 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ touch /usr/bin/grasshopper
 cp grasshopper /usr/bin/grasshopper
 chmod 777 /usr/bin/grasshopper
 chmod -R 777 /usr/share/grasshopper
+cp grasshopper.8.gz /usr/share/man/man8/
 rm /usr/share/grasshopper/install.sh
 echo "Thanks bro"
